adfa1da628fce60b9549d6349c06378f8b0a30f6
L-Forster <louisforster208@gmail.com>
2025-06-12 19:07:29 +0100

    conda packages

--- slurmjob.sh
+++ slurmjob.sh
@@ -1,6 +1,6 @@
 . ~/initMamba.sh
-conda activate py39
+conda activate Zero-TIG
 cd /user/home/ra2224/Zero-TIG
+conda env update -f environment.yml
 nvidia-smi
-conda install --file requirements.txt
 python run_pipeline.py --datasets RLV DID 3_SDSD --base_data_dir /user/work/gf19473/datasets/ --weights_dir ./weights/ --pretrain_weights_file BVI-RLV.pt --base_exp_dir ./PIPELINE_EXP --num_workers 0 --epochs 5
