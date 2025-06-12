fae855fa3df600c89551d921ed827230245692e6
L-Forster <louisforster208@gmail.com>
2025-06-12 19:13:43 +0100

    correct conda env

--- slurmjob.sh
+++ slurmjob.sh
@@ -1,6 +1,6 @@
 . ~/initMamba.sh
-conda activate Zero-TIG
-cd /user/home/ra2224/Zero-TIG
+conda activate ptlflow
+cd ./Zero-TIG/
 conda env update -f environment.yml
 nvidia-smi
-python run_pipeline.py --datasets RLV DID 3_SDSD --base_data_dir /user/work/gf19473/datasets/ --weights_dir ./weights/ --pretrain_weights_file BVI-RLV.pt --base_exp_dir ./PIPELINE_EXP --num_workers 0 --epochs 5
+python run_pipeline.py --datasets RLV DID_1080 3_SDSD --base_data_dir /user/work/gf19473/datasets/ --weights_dir ./weights/ --pretrain_weights_file BVI-RLV.pt --base_exp_dir ./PIPELINE_EXP --num_workers 0 --epochs 5
